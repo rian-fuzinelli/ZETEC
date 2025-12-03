bbc505022bef6fbae9fd7db40f1ecb088e01dbf5
rian-fuzinelli <riancaiofuzinelli12@gmail.com>
2025-12-03 19:10:00 -0300

    fix: ajustar endereço de rota

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -16,13 +15,16 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="ZETEC">
+        
+        <BrowserRouter
+          basename={import.meta.env.MODE === "production" ? "/ZETEC" : ""}
+        >
           <Routes>
             <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
